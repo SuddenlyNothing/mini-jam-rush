42fba9ad11b0ddafa3fe57ab7ea11422f1880769
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-09-03 15:49:47 -0700

    new doc

--- RushGame/Scenes/Cars/Car.gd
+++ RushGame/Scenes/Cars/Car.gd
@@ -4,13 +4,15 @@ class_name car
 onready var front_wheel := $FrontWheel
 onready var back_wheel := $BackWheel
 
-export(float) var wheel_torque : float = 1000
+export(float) var wheel_torque : float = 5000
 export(float) var torque : float = 1000
 
 func _physics_process(delta : float) -> void:
 	var horizontal := Input.get_action_strength("right") - Input.get_action_strength("left")
-#	set_applied_torque(torque * gas)
 #	wheel.set_applied_force(wheel_torque * gas)
+	if not front_wheel.get_colliding_bodies() or not back_wheel.get_colliding_bodies():
+		set_applied_torque(torque * horizontal)
+		
 	front_wheel.set_applied_torque(wheel_torque * horizontal)
 	back_wheel.set_applied_torque(wheel_torque * horizontal)
 
